f466926b6ab3acea66f7d67984bb24cae39163d8
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-07-19 11:10:17 +0200

    updated calculator

--- calc/calc.py
+++ calc/calc.py
@@ -6,42 +6,32 @@ thrust = np.sin(np.pi * 45 / 180)
 # meter
 lever = 0.5
 
-# motor forces and torques (fx, fz, mz)
-A = np.array([
+# motor forces and torques matrix (fx, fz, mz)
+MATRIX = np.array([
     [thrust, thrust, thrust, thrust],
-    # translation in y is not possible
     [-thrust, thrust, thrust, -thrust],
-    [thrust*lever, thrust*lever, thrust*-lever, thrust*-lever], # mz
+    [thrust*lever, thrust*lever, thrust*-lever, thrust*-lever]
 ])
 
 # max duty cycle
-u_max = 255
+max_duty_cycle = 255
 
 # max motor forces in X
-m = np.array([1, 1, 1, 1]) * u_max
-FX_MAX = np.max(np.dot(A, m))
+FX_MAX = np.max(np.dot(MATRIX, np.array([1, 1, 1, 1]) * max_duty_cycle))
 print("FX_MAX", FX_MAX)
 
 # max motor forces in Z
-m = np.array([-1, 1, 1, -1]) * u_max
-FZ_MAX = np.max(np.dot(A, m))
-print("FZ_MAX",FZ_MAX)
+FZ_MAX = np.max(np.dot(MATRIX, np.array([-1, 1, 1, -1]) * max_duty_cycle))
+print("FZ_MAX", FZ_MAX)
 
 # max motor torques in Z
-m = np.array([1, 1, -1, -1]) * u_max
-MZ_MAX = np.max(np.dot(A, m))
+MZ_MAX = np.max(np.dot(MATRIX, np.array([1, 1, -1, -1]) * max_duty_cycle))
 print("MZ_MAX", MZ_MAX)
 
 # pseudo inverse of matrix
-B = np.linalg.pinv(A)
-print(B)
+INVERSE = np.linalg.pinv(MATRIX)
+print("INVERSE", INVERSE)
 
-# example
-
-FX = FX_MAX
-FZ = 0
-MZ = MZ_MAX
-
-f = np.array([FX, FZ, MZ])
-m = np.dot(B, f)
-print(m)
+# example calculation
+f = np.array([FX_MAX, 0, MZ_MAX])
+print("example", np.dot(INVERSE, f))
