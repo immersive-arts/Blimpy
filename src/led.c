e4f2b69ea996a42175fd97ae2766812052b3f60e
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-11-18 17:06:49 +0100

    fmt

--- src/led.c
+++ src/led.c
@@ -9,14 +9,22 @@
 void led_init() {
   // prepare ledc timer config
   ledc_timer_config_t t = {
-      .bit_num = LEDC_TIMER_10_BIT, .freq_hz = 5000, .speed_mode = LEDC_HIGH_SPEED_MODE, .timer_num = LEDC_TIMER_0};
+      .bit_num = LEDC_TIMER_10_BIT,
+      .freq_hz = 5000,
+      .speed_mode = LEDC_HIGH_SPEED_MODE,
+      .timer_num = LEDC_TIMER_0,
+  };
 
   // configure ledc timer
   ESP_ERROR_CHECK(ledc_timer_config(&t));
 
   // prepare ledc channel config
   ledc_channel_config_t c = {
-      .duty = 1024, .intr_type = LEDC_INTR_DISABLE, .speed_mode = LEDC_HIGH_SPEED_MODE, .timer_sel = LEDC_TIMER_0};
+      .duty = 1024,
+      .intr_type = LEDC_INTR_DISABLE,
+      .speed_mode = LEDC_HIGH_SPEED_MODE,
+      .timer_sel = LEDC_TIMER_0,
+  };
 
   // configure red led
   c.gpio_num = LED_RED_PIN;
