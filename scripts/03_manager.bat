5b7ee3bc8ca94a84bab6b4a79aecd6755a86599f
IAS-ABSINTHE\IAAdmin <ia.space@zhdk.ch>
2021-08-27 15:47:03 +0200

    update scripts

--- scripts/03_manager.bat
+++ scripts/03_manager.bat
@@ -2,4 +2,5 @@ pip install paho-mqtt
 pip install numpy
 pip install osc4py3
 pip install scipy
-python %~dp0..\manager\manager.py --mqtt_host "10.128.96.250" --mqtt_port 1883 --osc_server "10.128.96.250" --osc_port 1880 --base_topic "manager"
+pip install pyyaml
+python %~dp0..\manager\manager.py --mqtt_host "10.128.96.189" --mqtt_port 1883 --osc_server "10.128.96.189" --osc_port 1880 --manager_base_topic "manager"
