7aa95c816e63d162376be375fbf60913cd1392f1
IAS-ABSINTHE\IAAdmin <ia.space@zhdk.ch>
2022-03-28 21:51:30 +0200

    Add scripts folder

--- scripts/03_manager.bat
+++ scripts/03_manager.bat
@@ -4,4 +4,4 @@ pip install osc4py3
 pip install scipy
 pip install pyyaml
 pip install pyquaternion
-python %~dp0..\manager\manager.py --mqtt_host "10.128.96.189" --mqtt_port 1883 --osc_server "10.128.96.189" --osc_port 1880 --manager_base_topic "manager"
+python %~dp0..\manager\manager.py --mqtt_host "10.128.96.189" --mqtt_port 1883 --osc_server "10.128.96.189" --osc_port 1880 --manager_base_topic "manager" --device_folder "devices"
